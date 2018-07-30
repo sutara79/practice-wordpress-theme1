3e88ddee9c4c1ca444f4c42acac9ef4ea48c1c3c
Yuusaku Miyazaki <toumin.m7@gmail.com>
2018-07-30 16:46:38 +0900

    Add JavaScript

--- functions.php
+++ functions.php
@@ -2,3 +2,12 @@
 require_once locate_template('lib/custom-menu.php');
 require_once locate_template('lib/copyright.php');
 require_once locate_template('lib/jumbotronImg.php');
+
+function my_enqueue_scripts() {
+  wp_enqueue_script( 
+    'main-script', 
+    get_template_directory_uri() . '/assets/js/main.js' 
+  );
+}
+
+add_action('wp_enqueue_scripts', 'my_enqueue_scripts');
