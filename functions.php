c057b8d838d8f844c4028f45091f58bab22c5dd1
Yuusaku Miyazaki <toumin.m7@gmail.com>
2018-08-03 11:07:19 +0900

    Hide admin-bar always

--- functions.php
+++ functions.php
@@ -3,11 +3,16 @@ require_once locate_template('lib/custom-menu.php');
 require_once locate_template('lib/copyright.php');
 require_once locate_template('lib/jumbotronImg.php');
 
+/**
+ * 独自のJavaScriptを使えるようにする
+ */
 function my_enqueue_scripts() {
   wp_enqueue_script( 
     'main-script', 
     get_template_directory_uri() . '/assets/js/main.js' 
   );
 }
-
 add_action('wp_enqueue_scripts', 'my_enqueue_scripts');
+
+// アドミンバーを非表示にする
+add_filter('show_admin_bar', '__return_false');
