3d4c7f27b004cccf8fc1e5315c763af26a918729
Yuusaku Miyazaki <toumin.m7@gmail.com>
2018-08-04 14:27:41 +0900

    Remove unnecessary code

--- header.php
+++ header.php
@@ -13,21 +13,23 @@
 <header class="header">
   <input type="checkbox" id="mobile-gnav__checkbox" class="mobile-gnav__checkbox">
   <div class="header__main">
-    <div class="container header__inner">
-      <h1 class="header__logo">
-        <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
-      </h1>
-      <div class="mobile-gnav__btn-wrapper">
-        <label for="mobile-gnav__checkbox" class="mobile-gnav__btn icon-menu"></label>
+    <div class="container">
+      <div class="header__inner">
+        <h1 class="header__logo">
+          <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+        </h1>
+        <div class="mobile-gnav__btn-wrapper">
+          <label for="mobile-gnav__checkbox" class="mobile-gnav__btn icon-menu"></label>
+        </div>
+        <nav class="pc-gnav">
+          <?php wp_nav_menu([
+            'container' => false,
+            'menu' => 'globalnavi',
+            'depth' => 3,
+            'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>'
+          ]); ?>
+        </nav>
       </div>
-      <nav class="pc-gnav">
-        <?php wp_nav_menu([
-          'container' => false,
-          'menu' => 'globalnavi',
-          'depth' => 3,
-          'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>'
-        ]); ?>
-      </nav>
     </div>
   </div>
   <nav class="mobile-gnav">
