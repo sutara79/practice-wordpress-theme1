a2718cb0f6c6a6a7246d705750d7717190c38e9e
Yuusaku Miyazaki <toumin.m7@gmail.com>
2018-09-01 14:11:46 +0900

    Modify .post__header

--- template-parts/main/contents/single.php
+++ template-parts/main/contents/single.php
@@ -1,7 +1,21 @@
 <?php the_post(); ?>
 <div class="post__header">
-    <span>カテゴリ：<?php the_category(', ') ?></span>
-    <span>投稿日：<?php echo get_the_date(); ?></span>
+    <div class="post__date">
+        <span class="post__date__created">
+            <?php _e('投稿') ?>: <?php echo get_the_date() ?>
+        </span>
+        <?php if (get_the_date() != get_the_modified_date()) : ?>
+            <span class="post__date__modified">
+                <?php _e('更新') ?>: <?php the_modified_date() ?>
+            </span>
+        <?php endif ?>
+    </div>
+    <div class="post__category">
+        <?php the_category() ?>
+    </div>
+    <div class="post__tags">
+        <?php the_tags('', '') ?>
+    </div>
 </div>
 <div class="post__body">
     <?php the_content(); ?>
