80d259141abb8f19f08dd35a241a6d6a9e2e169e
Yuusaku Miyazaki <toumin.m7@gmail.com>
2018-09-05 11:16:33 +0900

    CSS for archive

--- template-parts/main/contents/archive.php
+++ template-parts/main/contents/archive.php
@@ -6,12 +6,22 @@
  * @since 1.0.0
  */
 
-while (have_posts()) : the_post(); ?>
-    <li>
-        <span class="news-date"><?php the_time(get_option('date_format')) ?></span>
-        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-    </li>
-<?php endwhile; ?>
+?>
+<ol class="archive">
+    <?php while (have_posts()) : the_post(); ?>
+        <li class="archive__post">
+            <div class="archive__post__date">
+                <?php the_time(get_option('date_format')) ?>
+            </div>
+            <h3 class="archive__post__title">
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h3>
+            <div class="archive__post__excerpt">
+                <?php the_excerpt() ?>
+            </div>
+        </li>
+    <?php endwhile; ?>
+</ol>
 
 <?php
 the_posts_pagination( array(
--- template-parts/main/contents/home.php
+++ template-parts/main/contents/home.php
@@ -5,16 +5,5 @@
     ?>
 </div>
 <div>
-    <?php if (have_posts()) : ?>
-        <ul class="news-container">
-            <?php while (have_posts()) : the_post(); ?>
-                <li>
-                    <span class="news-date"><?php the_time(get_option('date_format')) ?></span>
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                </li>
-            <?php endwhile; ?>
-        </ul>
-    <?php else : ?>
-        <?php echo __('投稿記事はありません。'); ?>
-    <?php endif; ?>
+    <?php get_template_part('template-parts/main/contents/archive'); ?>
 </div>
